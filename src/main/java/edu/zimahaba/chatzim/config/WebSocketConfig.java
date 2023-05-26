c7a9b871cf2d2c25b6286489f844e3338a6c7f82
zimahaba <pmaquitorres@gmail.com>
2023-05-26 18:47:29 -0300

    connection on handle message

--- src/main/java/edu/zimahaba/chatzim/config/WebSocketConfig.java
+++ src/main/java/edu/zimahaba/chatzim/config/WebSocketConfig.java
@@ -1,12 +1,20 @@
 package edu.zimahaba.chatzim.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.zimahaba.chatzim.ws.ChatHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.Map;
 
 @Configuration
 @EnableWebSocket
@@ -14,11 +22,17 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(webSocketHandler(), "/chat");
+        registry.addHandler(webSocketHandler(), "/chat")
+                .setAllowedOrigins("*");
     }
 
     @Bean
     public WebSocketHandler webSocketHandler() {
-        return new ChatHandler();
+        return new ChatHandler(objectMapper());
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
     }
 }
--- src/main/java/edu/zimahaba/chatzim/ws/ChatHandler.java
+++ src/main/java/edu/zimahaba/chatzim/ws/ChatHandler.java
@@ -1,5 +1,7 @@
 package edu.zimahaba.chatzim.ws;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.zimahaba.chatzim.model.UserSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,21 +23,27 @@ public class ChatHandler extends TextWebSocketHandler {
 
     Logger log = LoggerFactory.getLogger(ChatHandler.class);
 
-    private static String CHANNEL_HEADER = "channel";
-    private static String USER_ID_HEADER = "userId";
+    private static String CHANNEL_KEY = "channel";
+    private static String USER_KEY = "userId";
 
     Map<String, List<UserSession>> channels = new ConcurrentHashMap<>();
 
+    private final ObjectMapper om;
+
+    public ChatHandler(ObjectMapper om) {
+        this.om = om;
+    }
+
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        String channel =  session.getHandshakeHeaders().getFirst(CHANNEL_HEADER);
-        String userId =  session.getHandshakeHeaders().getFirst(USER_ID_HEADER);
+        /*String channel =  session.getHandshakeHeaders().getFirst(CHANNEL_KEY);
+        String userId =  session.getHandshakeHeaders().getFirst(USER_KEY);
         if (hasText(channel) && hasText(userId)) {
             log.info("Connection established with user '{}' in channel '{}'", userId, channel);
 
             // TODO move set attributes to handshake interceptor?
-            session.getAttributes().put(CHANNEL_HEADER, channel);
-            session.getAttributes().put(USER_ID_HEADER, userId);
+            session.getAttributes().put(CHANNEL_KEY, channel);
+            session.getAttributes().put(USER_KEY, userId);
 
             List<UserSession> userSessions;
             if (channels.containsKey(channel)) {
@@ -46,27 +54,62 @@ public class ChatHandler extends TextWebSocketHandler {
 
             userSessions.add(new UserSession(userId, session));
             channels.put(channel, userSessions);
-        }
+        }*/
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        String channel = (String) session.getAttributes().get(CHANNEL_HEADER);
-        String userId =  (String) session.getAttributes().get(USER_ID_HEADER);
+        JsonNode jsonPayload = om.readTree(message.getPayload());
+        String method = jsonPayload.get("method").asText();
 
-        if (hasText(channel) && hasText(userId) && channels.containsKey(channel)) {
-            //log.info("Message received from '{}': {}", userId, message.getPayload());
-            channels.get(channel).forEach((userSession -> {
-                if (!userSession.getUserId().equals(userId)) {
-                    //log.info("Got user '{}', sending msg: {}", userId, message.getPayload());
-                    try {
-                        userSession.getWsSession().sendMessage(new TextMessage(message.getPayload()));
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            }));
+        if (method.equals("connect")) {
+            handleConnection(session, jsonPayload);
+        } else if (method.equals("chat")) {
+            handleMessage(session, jsonPayload);
         }
+    }
+
+    private void handleConnection(WebSocketSession session, JsonNode jsonPayload) throws IOException {
+        String channel = jsonPayload.get(CHANNEL_KEY).asText();
+        String userId = jsonPayload.get(USER_KEY).asText();
+        if (hasText(channel) && hasText(userId)) {
+            log.info("Connection established with user '{}' in channel '{}'", userId, channel);
+
+            session.getAttributes().put(CHANNEL_KEY, channel);
+            session.getAttributes().put(USER_KEY, userId);
+
+            List<UserSession> userSessions;
+            if (channels.containsKey(channel)) {
+                userSessions = channels.get(channel);
+            } else {
+                userSessions = new ArrayList<>();
+            }
+
+            userSessions.add(new UserSession(userId, session));
+            channels.put(channel, userSessions);
+            String connectionResponse = "{\"method\": \"connection\", \"username\":\"" + userId + "\"}";
+            log.info("Sending connection response: {}", connectionResponse);
+            session.sendMessage(new TextMessage(connectionResponse));
+        }
+    }
+
+    private void handleMessage(WebSocketSession session, JsonNode jsonPayload) {
+        //log.info("Message received from '{}': {}", userId, message.getPayload());
+        String channel = (String) session.getAttributes().get(CHANNEL_KEY);
+        String userId = (String) session.getAttributes().get(USER_KEY);
+        String message = jsonPayload.get("payload").asText();
+
+        String broadcastPayload = "{\"method\": \"chat\", \"username\":\"" + userId + "\", \"message\":\"" + message + "\"}";
+        channels.get(channel).forEach((userSession -> {
+            if (!userSession.getUserId().equals(userId)) {
+                log.info("Got user '{}', sending msg: {}", userId, message);
+                try {
+                    userSession.getWsSession().sendMessage(new TextMessage(broadcastPayload));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }));
     }
 
     @Override
