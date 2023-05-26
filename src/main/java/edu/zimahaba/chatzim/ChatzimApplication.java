d2d8762aeada27e77b73cb6816eb41d8b30191dc
zimahaba <pmaquitorres@gmail.com>
2023-05-25 21:23:39 -0300

    app class name

--- src/main/java/edu/zimahaba/chatzim/ChatzimApplication.java
+++ src/main/java/edu/zimahaba/chatzim/ChatzimApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebsocketApplication {
+public class ChatzimApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebsocketApplication.class, args);
+		SpringApplication.run(ChatzimApplication.class, args);
 	}
 
 }
